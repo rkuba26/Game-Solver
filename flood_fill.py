32da962c4158b5a17226f56a52928ad33f913d72
rkuba26 <129909448+rkuba26@users.noreply.github.com>
2023-05-17 16:19:41 -0400

    Add files via upload

---
+++ flood_fill.py
@@ -0,0 +1,158 @@
+def flood_fill(image, location, new_color):
+    """
+    Given an image, replace the same-colored region around a given location
+    with a given color.  Returns None but mutates the original image to
+    reflect the change.
+
+    Parameters:
+      * image: the image to operate on
+      * location: an (row, col) tuple representing the starting location of the
+                  flood-fill process
+      * new_color: the replacement color, as an (r, g, b) tuple where all values
+                   are between 0 and 255, inclusive
+    """
+    original_color = get_pixel(image, *location)
+    def get_neighbors(cell):
+        row,col = cell
+        return [(row+1,col), (row-1,col), (row,col+1), (row,col-1)]
+    to_color = [location]
+    visited = {location}
+    while to_color:
+        this_cell = to_color.pop(0)
+        print(to_color)
+        set_pixel(image,*this_cell,new_color)
+        for neighbor in get_neighbors(this_cell):
+            if neighbor not in visited and get_pixel(image,*neighbor) == original_color:
+                to_color.append(neighbor)
+                visited.add(neighbor)
+
+def find_path(image, start_location, goal_color):
+    safe_color = get_pixel(image, *start_location)
+    def get_neighbors(cell):
+        row, col = cell 
+        potential_neighbors = [(row+1,col), (row-1,col), (row,col+1), (row,col-1)]
+        return [(nr,nc) for nr, nc in potential_neighbors if 0 <= nr < get_height(image) and 0 <= nc < get_width(image)]
+    def color(current_path):
+        print(len(current_path))
+        for i in current_path:
+            set_pixel(image, *i, (255, 0, 0))
+    possible_paths = [[start_location]]
+    visited = {start_location}
+    while possible_paths: 
+        current_path = possible_paths.pop(0) 
+        for neighbor in get_neighbors(current_path[-1]):
+            if get_pixel(image,*neighbor) == goal_color:
+                current_path.append(neighbor)
+                print(current_path)
+                color(current_path)
+                return current_path
+            elif neighbor in visited: 
+                continue 
+            elif get_pixel(image,*neighbor) == pygame.K_k:
+                visited.add(neighbor)
+            elif get_pixel(image,*neighbor) == safe_color:
+                visited.add(neighbor)
+                new_path = current_path.copy()
+                new_path.append(neighbor)
+                possible_paths.append(new_path)
+
+##### IMAGE REPRESENTATION WITH SIMILAR ABSTRACTIONS TO LAB 1 AND 2
+
+
+def get_width(image):
+    return image.get_width() // SCALE
+
+
+def get_height(image):
+    return image.get_height() // SCALE
+
+
+def get_pixel(image, row, col):
+    try:
+        color = image.get_at((col * SCALE, row * SCALE))
+        return (color.r, color.g, color.b)
+    except IndexError:
+        return None
+
+
+def set_pixel(image, row, col, color):
+    loc = row * SCALE, col * SCALE
+    c = pygame.Color(*color)
+    for i in range(SCALE):
+        for j in range(SCALE):
+            image.set_at((loc[1] + i, loc[0] + j), c)
+    ## comment out the two lines below to avoid redrawing the image every time
+    ## we set a pixel
+    screen.blit(image, (0, 0))
+    pygame.display.flip()
+
+
+##### USER INTERFACE CODE
+##### DISPLAY AN IMAGE AND CALL flood_fill WHEN THE IMAGE IS CLICKED
+
+import os
+import sys
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+import pygame
+
+from pygame.locals import *
+
+COLORS = {
+    pygame.K_r: (255, 0, 0),
+    pygame.K_w: (255, 255, 255),
+    pygame.K_k: (0, 0, 0),
+    pygame.K_g: (0, 255, 0),
+    pygame.K_b: (0, 0, 255),
+    pygame.K_c: (0, 255, 255),
+    pygame.K_y: (255, 230, 0),
+    pygame.K_p: (179, 0, 199),
+    pygame.K_o: (255, 77, 0),
+    pygame.K_n: (66, 52, 0),
+    pygame.K_e: (152, 152, 152),
+}
+
+COLOR_NAMES = {
+    pygame.K_r: "red",
+    pygame.K_w: "white",
+    pygame.K_k: "black",
+    pygame.K_g: "green",
+    pygame.K_b: "blue",
+    pygame.K_c: "cyan",
+    pygame.K_y: "yellow",
+    pygame.K_p: "purple",
+    pygame.K_o: "orange",
+    pygame.K_n: "brown",
+    pygame.K_e: "grey",
+}
+
+SCALE = 1
+IMAGE = "huge_maze.png"
+
+pygame.init()
+image = pygame.image.load(IMAGE)
+dims = (image.get_width() * SCALE, image.get_height() * SCALE)
+screen = pygame.display.set_mode(dims)
+image = pygame.transform.scale(image, dims)
+screen.blit(image, (0, 0))
+pygame.display.flip()
+initial_color = pygame.K_g
+cur_color = COLORS[initial_color]
+# print("current color:", COLOR_NAMES[initial_color])
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN:
+            if event.key in COLORS:
+                cur_color = COLORS[event.key]
+                print("current color:", COLOR_NAMES[event.key])
+            elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit(0)
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            # flood_fill(image, (event.pos[1] // SCALE, event.pos[0] // SCALE), cur_color)
+            find_path(image, (event.pos[1] // SCALE, event.pos[0] // SCALE), cur_color)
+            screen.blit(image, (0, 0))
+            pygame.display.flip()
---
+++ lab.py
@@ -0,0 +1,250 @@
+"""
+6.1010 Spring '23 Lab 4: Snekoban Game
+"""
+
+# import json
+# import typing
+# NO ADDITIONAL IMPORTS!
+direction_vector = {
+    "up": (-1, 0),
+    "down": (+1, 0),
+    "left": (0, -1),
+    "right": (0, +1),
+}
+def initial():
+    """
+    Part of new_game: makes empty sets for each key
+    """
+    pieces = {"player": set(), "target": set(), \
+              "wall": set(), "computer": set(), "open": set()}
+    return pieces 
+
+def new_game(level_description):
+    """
+    Given a description of a game state, create and return a game
+    representation of your choice.
+
+    The given description is a list of lists of lists of strs, representing the
+    locations of the objects on the board (as described in the lab writeup).
+
+    For example, a valid level_description is:
+
+    [
+        [[], ['wall'], ['computer']],
+        [['target', 'player'], ['computer'], ['target']],
+    ]
+
+    The exact choice of representation is up to you; but note that what you
+    return will be used as input to the other functions.
+    """
+    data = initial()
+    rows, cols = len(level_description),len(level_description[0])
+    for row in range(rows):
+        for col in range(cols):
+            if not level_description[row][col]:
+                continue
+            for piece in level_description[row][col]:
+                data[piece].add((row,col))
+    data["x"] = (rows,cols)
+    return data
+    
+def victory_check(game):
+    """
+    Checks that if there are computers and targets,
+    that each computer is on a target
+    If it isn't, the game isn't solved
+    """
+    compare = game["computer"] ^ game["target"]
+    if not compare and game["computer"]: 
+        return True
+    return False
+
+def change(coord,diff):
+    """
+    Find the coordinate adjacent to it
+    """
+    return (coord[0]+diff[0],coord[1]+diff[1])
+
+def add_rem(sect,rem,add):
+    """
+    Changing the position of the player or computer
+    """
+    section = sect.copy()
+    section.remove(rem)
+    section.add(add)
+    return section
+
+def step_game(game, direction):
+    """
+    Two possible ways a player can move:
+    1) The movement is to an empty space
+    An empty space is defined as not having a computer or wall
+    2) The movement pushes a computer into an empty space
+    """
+    wall_comp = lambda x: x in revised["wall"] or x in revised["computer"]
+    computer = lambda x: x in revised["computer"]
+    revised = game.copy()
+    # Find the coordinate of the player
+    (coord,) = revised["player"]
+    add = direction_vector[direction]
+    # New is the coordinate that the player wants to move to
+    new = change(coord,add)
+    # If New is empty space
+    if not wall_comp(new) and not computer(new):
+        revised["player"] = add_rem(game["player"],coord,new)
+    elif computer(new):
+        # Extra is the coordinate want to move computer to
+        extra = change(new,add)
+        # As long as extra is an empty space
+        if not wall_comp(extra):
+            revised["computer"] = add_rem(game["computer"],new,extra)
+            revised["player"] = add_rem(game["player"],coord,new)
+    return revised
+
+def dump_game(game):
+    """
+    Given a game representation (of the form returned from new_game), convert
+    it back into a level description that would be a suitable input to new_game
+    (a list of lists of lists of strings).
+
+    This function is used by the GUI and the tests to see what your game
+    implementation has done, and it can also serve as a rudimentary way to
+    print out the current state of your game for testing and debugging on your
+    own.
+    """
+    rows,cols = game["x"]
+    representation = []
+    for row in range(rows):
+        curr_row = [[] for _ in range(cols)]
+        representation.append(curr_row)
+    for key in game: 
+        if key in {"open", "x"}: 
+            continue
+        for row,col in game[key]: 
+            representation[row][col].append(key)
+    return representation
+
+def neighbors(node,wall):
+    neighbor = []
+    info = {"player": {node[0]}, "computer": node[1], "wall": wall}
+    for direction in direction_vector:
+        neighbor.append(make_hash(step_game(info,direction),direction))
+    return neighbor
+
+def make_hash(game,direction = None):
+    (player,) = game["player"]
+    computers = game["computer"]
+    return (player,computers,direction)
+
+def seen_hash(made):
+    return made[0] + (frozenset(made[1]),) + (made[2],)
+
+def test(touching,coord,target):
+    """
+    The testing part of the terminate function
+    """
+    set1 = {(-1,0),(+1,0)}
+    set2 = {(0,-1),(0,+1)}
+    if len(touching) == 2 and not touching ^ set1 or not touching ^ set2:
+        return True 
+    elif len(touching) > 2 and coord not in target: 
+        return True
+    return False 
+
+def terminate(neighbor,wall,target):
+    """
+    Looks thru all the coordinates adjacent to
+    one of the computer sides
+    If a computer is in a corner of a wall,
+    This pathway will never solve the game, so
+    it will return True (meaning it should terminate)
+    """
+    for coord in neighbor[1]: 
+        adj = adjacent(coord)
+        touching = {th for th in adj if th in wall}
+        if test(touching,coord,target):
+            return True
+    return False 
+
+def adjacent(coord):
+    adjac = []
+    add = list(direction_vector.values())
+    for direction in add: 
+        adjac.append(change(coord,direction))
+    return adjac
+
+def path(current):
+    list1 = []
+    for i in range(1, len(current)):
+        list1.append(current[i][2])
+    return list1
+
+def solve_puzzle(game):
+    """
+    BFS
+    Also checks if already solved at beginning
+    """
+    solved = lambda comp: not comp ^ target
+    target = game["target"]
+    wall = game["wall"]
+    transform = make_hash(game)
+    if solved(transform[1]):
+        return []
+    queue = [(transform,)]
+    seen = {seen_hash(transform)}
+    while queue: 
+        look_at = queue.pop(0)
+        for neighbor in neighbors(look_at[-1],wall):
+            if seen_hash(neighbor) in seen:
+                continue
+            elif solved(neighbor[1]):
+                return path(look_at + (neighbor,))
+            seen.add(seen_hash(neighbor))
+            if not terminate(neighbor,wall,target): 
+                new_path = look_at + (neighbor,)
+                queue.append(new_path)
+    return None
+
+if __name__ == "__main__":
+    puzzle = [
+  [[], [], ["wall"], ["wall"], ["wall"], ["wall"], [], [], []],
+  [
+    ["wall"],
+    ["wall"],
+    ["wall"],
+    [],
+    [],
+    ["wall"],
+    ["wall"],
+    ["wall"],
+    ["wall"]
+  ],
+  [["wall"], [], [], [], [], [], ["computer"], [], ["wall"]],
+  [["wall"], [], ["wall"], [], [], ["wall"], ["computer"], [], ["wall"]],
+  [
+    ["wall"],
+    [],
+    ["target"],
+    [],
+    ["target"],
+    ["wall"],
+    ["player"],
+    [],
+    ["wall"]
+  ],
+  [
+    ["wall"],
+    ["wall"],
+    ["wall"],
+    ["wall"],
+    ["wall"],
+    ["wall"],
+    ["wall"],
+    ["wall"],
+    ["wall"]
+  ]
+    ]
+    print(solve_puzzle(new_game(puzzle)))
+
+
+
